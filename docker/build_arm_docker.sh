f8943dd1743c4c480db43e046c42beae07fd6eb5
bbouffaut <baptiste.bouffaut@gmail.com>
2025-05-28 18:30:56 +0200

    build a docker image to run mlc-llm locallly from source

---
+++ docker/build_arm_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TAG=$1
+PLATFORM=$2
+
+# llama_cpp + nanollm container based only
+# docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.10-llama_cpp-nanollm-cuda:$TAG -f ./docker/Dockerfile .
+
+# llama-cpp + mlc_llm
+docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.10-cuda:$TAG -f ./docker/Dockerfile .
